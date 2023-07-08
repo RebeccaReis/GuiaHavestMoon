13feaac9298fc27bf3e42cbd02cd7ad8b35158bc
Rebecca vitoria Santos reis <111397529+RebeccaReis@users.noreply.github.com>
2023-07-08 22:22:01 +0000

    responsividade quase pronta e imagens pag inicial okay

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,22 @@
 import React from "react";
+
+import logo from "../../images/logo.png";
+import placa from "../../images/placa.webp";
+import start from "../../images/start.png";
+import personagem from "../../images/personagem.webp";
+
 import "../Body/Body.css";
 
 const Body = () => {
   return (
            <div className="body-div">
               <div className="box-content">
-                    <img className="logo" src="."/>
+                    <div className="logo-container"><img className="logo" src={logo}/></div>
+                    <div className="placa-container">
+                        <img className="placa" src={placa}/ >
+                            <img className="start" src={start}/>
+                            <img className="personagem" src={personagem}/>
+                    </div>
               </div>
            </div>
   )
